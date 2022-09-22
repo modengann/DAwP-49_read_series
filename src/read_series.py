5fdf423bf42840dafe954c1570eada438b3ebd47
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:47:57 -0400

    init

---
+++ src/read_series.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def read_series():
+    pass          
+
+
+def main():
+    result = read_series()
+    print(result.values)
+
+if __name__ == "__main__":
+    main()
